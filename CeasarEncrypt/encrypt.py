bebaa9caee72da5c243a1c6240f53aa2cb33f890
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-05-12 11:23:53 +0800

    Qrcode

--- CeasarEncrypt/encrypt.py
+++ CeasarEncrypt/encrypt.py
@@ -1,10 +1,19 @@
 import string
 
 # VIa class
-def ceasar(text, shift, alphabets):
+def caesar(text, shift, alphabets):
 
-    def shift(alphabet):
+    def shift_alphabet(alphabet):
         return alphabet[shift:] + alphabet[:shift]
+
+    shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+    final_alphabet = ''.join(alphabets)
+    final_shifted_alphabet = ''.join(shifted_alphabets)
+    table = str.maketrans(final_alphabet, final_shifted_alphabet)
+    return text.translate(table)
+
+plain_text = "This is a new test. Hello World!"
+print(caesar(plain_text, 7, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
 
 
 
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pythonapps!")
+
+
+if __name__ == "__main__":
+    main()
