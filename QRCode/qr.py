e0986f4cf94cc58787fcfa8db8d595f6ac340240
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-05-14 11:15:57 +0800

    FTPUpdate

--- QRCode/qr.py
+++ QRCode/qr.py
@@ -1,2 +1,22 @@
-import QRCode
+import qrcode   # you have to install pillow also
+import qrcode.image.svg
 
+factory = qrcode.image.svg.SvgPathImage
+svg_img = qrcode.make("Hello World! This is Fenris!", image_factory=factory)
+svg_img.save("myqrcodesvg.svg")
+
+# Create a QR code with the specified data
+# img = qrcode.make("Hello World! This is Fenris!")
+# img.save("myqrcode.png")
+
+# Create a QR code with custom settings
+# qr = qrcode.QRCode(version=1,
+#                    error_correction=qrcode.constants.ERROR_CORRECT_L,
+#                    box_size=20,
+#                    border=2)
+
+# qr.add_data("https://www.neuralnine.com/books")
+# qr.make(fit=True)
+
+# img = qr.make_image(fill_color="black", back_color="white")
+# img.save("advancedQR.png")
