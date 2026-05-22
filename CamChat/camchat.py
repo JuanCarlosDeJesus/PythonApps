89d8f39306116d9db63f62f1952383991dc09ea1
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-05-22 16:29:20 +0800

    CamChat

--- CamChat/camchat.py
+++ CamChat/camchat.py
@@ -1,1 +1,23 @@
 # Camera Chat
+from vidstream import StreamingServer
+from vidstream import CameraClient
+
+import threading
+import time
+
+receiving = StreamingServer('192.168.56.1', 9999) # myip.is 222.127.93.32 public address
+sending = CameraClient('192.168.254.196', 9999) # laptop addy
+
+t1 = threading.Thread(target=receiving.start_server)
+t1.start()
+
+time.sleep(2)
+
+t2 = threading.Thread(target=sending.start_stream)
+t2.start()
+
+while input("") != "STOP":
+    continue
+
+receiving.stop_server()
+sending.stop_stream()
