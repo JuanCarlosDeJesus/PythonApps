5f61c02ca40344ae813b4770341185d730d191b7
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-05-28 15:54:30 +0800

    GuiPokedex

--- Audiobook/audio.py
+++ Audiobook/audio.py
@@ -1,1 +1,25 @@
 # Audiobook
+import pyttsx3
+import PyPDF2
+
+with open('loremipsum.pdf', 'rb') as book:
+
+    full_text = ""
+
+    reader = PyPDF2.PdfReader(book)
+
+    audio_reader = pyttsx3.init()
+    audio_reader.setProperty("rate", 100)
+
+    for page in range(len(reader.pages)):
+        next_page = reader.pages[page]
+        content = next_page.extract_text()
+        full_text += content
+
+        # .say = audio reader
+        # audio_reader.say(content)
+        # audio_reader.runAndWait()
+
+    # .save_to_file = saves read file to text
+    audio_reader.save_to_file(full_text, "myaudio.mp3")
+    audio_reader.runAndWait()
