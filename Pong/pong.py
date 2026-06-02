eabca88526ec0f6856014cd8813504b6bbe6930d
JuanCarlos.DeJesus <45457547+JuanCarlosDeJesus@users.noreply.github.com>
2026-06-02 15:53:07 +0800

    TwitterBot

--- Pong/pong.py
+++ Pong/pong.py
@@ -2,11 +2,11 @@
 import pygame
 
 # constants and variables
-white = (255,255,255)
-black = (0,0,0)
+WHITE = (255,255,255)
+BLACK = (0,0,0)
 
-width = 600
-height = 600
+WIDTH = 600
+HEIGHT = 600
 
 pygame.init()
 game_font = pygame.font.SysFont('Ubuntu', 40)
@@ -46,7 +46,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 def draw_objects():
     pygame.draw.rect(screen, WHITE, (int(p1_x_pos), int(p1_y_pos), paddle_width, paddle_height))
     pygame.draw.rect(screen, WHITE, (int(p2_x_pos), int(p2_y_pos), paddle_width, paddle_height))
-    pygame.draw,circle(screen, WHITE, (ball_x_pos, ball_y_pos), ball_width)
+    pygame.draw.circle(screen, WHITE, (ball_x_pos, ball_y_pos), ball_width)
     score = game_font.render(f"{str(p1_score)} - {str(p2_score)}", False, WHITE)
     screen.blit(score, (WIDTH / 2, 30))
 
@@ -63,3 +63,73 @@ def apply_player_movement():
     elif p2_down:
         p2_y_pos = min(p2_y_pos + paddle_speed, HEIGHT - paddle_height)
 
+def apply_ball_movement():
+    global ball_x_pos
+    global ball_y_pos
+    global ball_x_vel
+    global ball_y_vel
+    global p1_score
+    global p2_score
+
+    if (ball_x_pos + ball_x_vel < p1_x_pos + paddle_width) and (p1_y_pos < ball_y_pos + ball_y_vel + ball_width < p1_y_pos + paddle_height):
+        ball_x_vel = -ball_x_vel
+        ball_y_vel = (p1_y_pos + paddle_height / 2 - ball_y_pos) / 15
+        ball_y_vel = -ball_y_vel
+    elif ball_x_pos + ball_x_vel < 0:
+        p2_score += 1
+        ball_x_pos = WIDTH / 2
+        ball_y_pos = HEIGHT / 2
+        ball_y_vel = 10
+        ball_y_vel = 0
+    if (ball_x_pos + ball_x_vel > p2_x_pos - paddle_width) and (p2_y_pos < ball_y_pos + ball_y_vel + ball_width < p2_y_pos + paddle_height):
+        ball_x_vel = -ball_x_vel
+        ball_y_vel = (p2_y_pos + paddle_height / 2 - ball_y_pos) /15
+        ball_y_vel = -ball_y_vel
+    elif ball_x_pos + ball_x_vel > HEIGHT:
+        p1_score += 1
+        ball_x_pos = WIDTH / 2
+        ball_y_pos = HEIGHT / 2
+        ball_y_vel = 10
+        ball_y_vel = 0
+    if ball_y_pos + ball_y_vel > HEIGHT or ball_y_pos + ball_y_vel < 0:
+        ball_y_vel = -ball_y_vel
+
+    ball_x_pos += ball_x_vel
+    ball_y_pos += ball_y_vel
+
+pygame.display.set_caption("Python Pong v0.1")
+screen.fill(BLACK)
+pygame.display.flip()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            if event.key == pygame.K_w:
+                p1_up = True
+            if event.key == pygame.K_s:
+                p1_down = True
+            if event.key == pygame.K_UP:
+                p2_up = True
+            if event.key == pygame.K_DOWN:
+                p2_down = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_w:
+                p1_up = False
+            if event.key == pygame.K_s:
+                p1_down = False
+            if event.key == pygame.K_UP:
+                p2_up = False
+            if event.key == pygame.K_DOWN:
+                p2_down = False
+
+    screen.fill(BLACK)
+    apply_player_movement()
+    apply_ball_movement()
+    draw_objects()
+    pygame.display.flip()
+    pygame.time.delay(delay)
